0d11e4d93c81ee9a5bf158eedbd56b05587e461b
itsdeeptiyadav <deeptiy795@gmail.com>
2024-03-26 16:54:55 +0530

    ..

---
+++ class.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class hero{
+    public:
+    int health;
+    char level;
+};
+int main(){
+    hero ramesh;
+    ramesh.health=70;
+    cout<<ramesh.health;
+
+}
