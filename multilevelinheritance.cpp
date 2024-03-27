3bb9a0f9347879d0dabb151ce99df5c1d44e8d0c
itsdeeptiyadav <deeptiy795@gmail.com>
2024-03-28 01:52:26 +0530

    ..

---
+++ multilevelinheritance.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class animals{
+    public:
+    int age;
+    int weight;
+    public:
+    void speaking(){
+        cout<<"is speaking"<<endl;
+    }
+};
+class dogs:public animals{
+    public:
+    void breed(){
+        cout<<"go to other func for breed"<<endl;
+    }
+};
+class germansepherd: public dogs{
+
+};
+int main(){
+    germansepherd g;
+    g.speaking();
+}
