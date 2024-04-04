237b2d623e6c5a6f0b06f4ad598e68fc86315751
itsdeeptiyadav <deeptiy795@gmail.com>
2024-04-04 22:50:06 +0530

    ...

--- singlyll.cpp
+++ singlyll.cpp
@@ -9,6 +9,12 @@ class node{
         this->next=NULL;
 
     }
+    ~node(){
+        if(this->next!=NULL){
+            delete next;
+            this->next=NULL;
+        }
+    }
 };
 
 void insertdata(node* &head,int d){
@@ -49,12 +55,35 @@ void insertatmid(node *&head,node* &tail,int d,int position){
     nodetoinsert->next=temp->next;
     temp->next=nodetoinsert;
 }
+void deletion(node* &head,int position){
+    node * temp=head;
+    int cnt=1;
+    if(position==1){
+        node * temp=head;
+        head=head->next;
+        temp->next=NULL;
+        delete temp;
+    }
+    else{
+       node * curr=head;
+       node *prev=NULL;
+       while(cnt<=position-1){
+        prev=curr;
+        curr=curr->next;
+        cnt++;
+        }
+    prev->next=curr->next;
+    curr->next=NULL;
+    delete curr;
+    }
+}
 int main(){
-    node* node1=new node(10);
+    node* node1=new node(9);
     node *head=node1;
     node*tail=node1;
     insertdata(head,12);
     insertattail(tail,25);
     insertatmid(head,tail,7,1);
+    deletion(head,1);
     print(head);
 }
