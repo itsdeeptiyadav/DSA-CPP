6a5f6648ede37c51100f2621c2da4f7934c905cd
itsdeeptiyadav <deeptiy795@gmail.com>
2024-05-28 15:06:25 +0200

    ..

--- llcircularornot.cpp
+++ llcircularornot.cpp
@@ -91,6 +91,25 @@ bool iscircular(node * head){
     }
     return false;
 }
+node * floyddetect(node* head){
+    if(head==NULL){
+        return NULL;
+    }
+    node * slow=head;
+    node* fast=head;
+    while(slow!=NULL&&fast!=NULL){
+        fast=fast->next;
+        if(fast!=NULL){
+            fast=fast->next;
+        }
+        slow=slow->next;
+        if(slow==fast){
+            return slow;
+        }
+    }
+    return NULL;
+
+}
 int main(){
     node* tail=NULL;
     
