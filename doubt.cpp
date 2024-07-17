a1c7d7a28578dfb31aa8b88f4dcba93c552e18b1
itsdeeptiyadav <deeptiy795@gmail.com>
2024-07-18 00:06:47 +0530

    ...

---
+++ doubt.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s="abcd";
+    cout<<s.substr(0,2);
+    for(int i=-2;i<0;i++){
+        cout<<i<<endl;
+    }
+}
--- recursion.cpp
+++ recursion.cpp
@@ -1,22 +1,43 @@
 #include<iostream>
 using namespace std;
-int power(int n,int m){
+double power(double n,int m){
     if(m==0){
         return 1;
     }
     return n*power(n,m-1);
 }
-int fact(int n){
-    if(n==0){//base case
-        return 1;
+double myPow(double x, int n){
+        if(n==0){
+            return 1;
+        }
+        return x*myPow(x,n-1);
+          long long N = n;
+    if (N < 0) {
+        x = 1 / x;
+        N = -N;
     }
-    
-    return n*fact(n-1);
-    
+    double result = 1;
+    double current_product = x;
+    for (long long i = N; i > 0; i /= 2) {
+        if (i % 2 == 1) {
+            result *= current_product;
+        }
+        current_product *= current_product;
+    }
+    return result;
 }
+// int fact(int n){
+//     if(n==0){//base case
+//         return 1;
+//     }
+    
+//     return n*fact(n-1);
+    
+// }
 int main(){
-    int n=6;
-    cout<<fact(n)<<endl;
-    int m=5;
+    double n=2;
+    
+    int m=10;
+    cout<<myPow(n,m)<<endl;
     cout<<power(n,m);
 }
