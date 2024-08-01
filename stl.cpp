ec41dcb9a8d98ee7207c3ed9e42416b160dd72d5
itsdeeptiyadav <deeptiy795@gmail.com>
2024-08-01 23:06:18 +0530

    ...

--- stl.cpp
+++ stl.cpp
@@ -2,7 +2,7 @@
 #include<array>
 using namespace std;
 int main(){
-    array<int,4>a={1,2,3,4};
+    array<int,4>a={1,2,8,4};
     int size=a.size();
     for(int i=0;i<size;i++){
         cout<<a[i]<<endl;
