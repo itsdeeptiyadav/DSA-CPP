527fee2597fb9eae8d66a6dc09d545150490eac4
itsdeeptiyadav <deeptiy795@gmail.com>
2024-08-06 01:03:03 +0530

    ...

---
+++ stack.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+class stack{
+    public:
+    int *arr;
+    int top;
+    int size;
+    stack(int size){
+        this->size=size;
+        arr=new int(size);
+        top=-1;
+    }
+    void push
+
+};
+
+int main(){
+    // stack <int> s;
+    // s.push(1);
+    // s.push(20);
+    // s.pop();
+    // cout<<s.top()<<endl;
+    // cout<<s.size();
+
+}
