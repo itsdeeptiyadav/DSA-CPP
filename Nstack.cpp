a6199be8ae4ac3c83a76b58fa7889d4b5d90537a
itsdeeptiyadav <deeptiy795@gmail.com>
2024-09-23 22:49:14 +0530

    ...

---
+++ Nstack.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+class solution{
+    int *arr;
+    int * top;
+    int * next;
+    int n,s;
+    int freespot;
+public:
+   int stackify(int N,int S){
+
+    }
+
+
+};
