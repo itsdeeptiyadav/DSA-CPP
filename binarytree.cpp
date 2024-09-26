78ab5e63e9e2cdf84f6398da36319d011acc6b41
itsdeeptiyadav <deeptiy795@gmail.com>
2024-09-26 23:43:03 +0530

    ...

--- binarytree.cpp
+++ binarytree.cpp
@@ -1,1 +1,13 @@
-#include 
+#include <iostream>
+using namespace std;
+class node{
+    public:
+        int data;
+        node *left;
+        node * right;
+    node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
