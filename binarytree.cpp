62e70738bc2cd93d280f4d342b5d1e28c3670972
itsdeeptiyadav <deeptiy795@gmail.com>
2024-09-27 22:34:58 +0530

    ...

--- binarytree.cpp
+++ binarytree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<queue>
 using namespace std;
 class node{
     public:
@@ -11,3 +12,43 @@ class node{
         this->right=NULL;
     }
 };
+node *buildtree(node * root){
+    cout<<"enter the data"<<endl;
+    int data;
+    cin>>data;
+    node * root= new node(data);
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"enter the left node";
+    root->left=buildtree(root->left);
+    cout<<"enter the right node";
+    root->right=buildtree(root->right);
+    return root;
+
+}
+void levelOrderTree(node* root){
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node *temp=q.front();
+        q.pop();
+        if(temp==NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+        }
+        if(temp->left){
+            q.push(temp->left);
+
+        }
+        if(temp->right){
+            q.push(temp->right);
+        }
+    }
+}
